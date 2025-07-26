1d368c4575e0796ecaee44a2ed737d719a888c85
mahongyin <1976222027@qq.com>
2025-07-26 23:06:35 +0800

    https://github.com/NanoHttpd/nanohttpd/pull/649 MultipartFormData顺序问题 https://github.com/NanoHttpd/nanohttpd/pull/633 升级commons-fileupload:commons-fileupload 1.3.3

--- core/src/test/java/org/nanohttpd/junit/protocols/http/HttpServerTest.java
+++ core/src/test/java/org/nanohttpd/junit/protocols/http/HttpServerTest.java
@@ -47,6 +47,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.util.Collections;
 import java.io.StringReader;
 import java.net.InetAddress;
 import java.util.ArrayList;
@@ -230,7 +231,9 @@ public class HttpServerTest {
 
                     try {
                         session.parseBody(this.files);
-                        for (String key : files.keySet()) {
+                        List<String> keyList = new ArrayList<>(files.keySet());
+                        Collections.sort(keyList);
+                        for (String key : keyList) {
                             responseMsg.append(key);
                         }
                     } catch (Exception e) {
